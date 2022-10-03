1be1727a7818da6e68b6ac185cc4bf113e1ec558
Sudhanshu <67652795+sudhanshublah@users.noreply.github.com>
2022-10-03 14:18:21 +0530

    Update allowed_ip.tf

--- allowed_ip.tf
+++ allowed_ip.tf
@@ -5,7 +5,7 @@ resource "aws_wafv2_ip_set" "site24_7_ipset" {
   scope = "REGIONAL"
   ip_address_version = "IPV4"
   addresses = [
-    "103.139.191.33/32"
+    "x.x.x.x/32"
   ]
 
   tags = merge(local.common_tags, {"Name": "${local.project_name_prefix}-site24x7-${var.ipset_whitelist_name}"})
